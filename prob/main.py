a735e4ffcf9dc904554b73319ba1d73c105306dd
its-Nishant-78 <nishantKu788@gmail.com>
2024-10-06 15:41:23 +0530

    Add files via upload

---
+++ prob/main.py
@@ -0,0 +1,30 @@
+import numpy as np
+np.random.seed(42)
+puppies=np.array([1,0,0,1,0,1,0,1,1,0,0,1,0,0,1,0,1,0,1,1,0,0,])
+p=puppies.mean()
+print("Mean",p)
+print("standerd deviation",puppies.std())
+print("variance",puppies.var())
+np.random.choice(puppies,size=(1,5), replace=True)
+np.random.choice(puppies,size=(1,5), replace=True).mean()
+print('\nsampling Disribution with size 5 \n')
+sample_props=[]
+for i in range(10000):
+    sample =np.random.choice(puppies,5,replace=True)
+    sample_props.append(sample.mean())
+sample_props = np.array(sample_props)
+sm=sample_props.mean()
+print("Mean",sample_props.mean())
+print("standard Drviation",sample_props.std())
+print("variance",sample_props.var())
+print("\nsampling Distrubution with size 20 \n")
+twenty_sample_props=[]
+for i in range(10000):
+    sample =np.random.choice(puppies,20,replace=True)
+    twenty_sample_props.append(sample.mean())
+twenty_sample_props= np.array(sample_props)
+sm=sample_props.mean()
+print("Mean",twenty_sample_props.mean())
+print("standard Drviation",twenty_sample_props.std())
+print("variance",twenty_sample_props.var())
+print("\nsampling Distrubution with size 20 \n")
